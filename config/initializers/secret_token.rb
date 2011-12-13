2fffc6149dc202bb3e8c00aa49b9ba662a004616
Bashanta Dahal <dahal_bsd@yahoo.com>
2011-12-13 22:33:27 +0545

    pj-tracker devise implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PjTracker::Application.config.secret_token = 'f3e07ab6d108d539e42d300af04eab36c193cf37658c1f0170862a703642cb43341db576999bfa5720cc895a92cebf7431c940bc260b366454f743467cb543c4'
